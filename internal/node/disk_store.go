44a6fbe74e3a4bd6c651ccd548bdf6f7798975ee
pranav718 <raypranav718@gmail.com>
2026-08-26 23:47:55 +0530

    feat: implement local disk storage engine for node chunks

---
+++ internal/node/disk_store.go
@@ -0,0 +1,141 @@
+package node
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+var (
+	ErrChunkNotFound = errors.New("chunk not found on node")
+	ErrInvalidChunkID = errors.New("invalid chunk identifier")
+)
+
+type DiskStore struct {
+	mu         sync.RWMutex
+	baseDir    string
+	maxCapacity int64
+}
+
+func NewDiskStore(baseDir string, maxCapacity int64) (*DiskStore, error) {
+	if err := os.MkdirAll(baseDir, 0755); err != nil {
+		return nil, err
+	}
+	return &DiskStore{
+		baseDir:    baseDir,
+		maxCapacity: maxCapacity,
+	}, nil
+}
+
+func (ds *DiskStore) chunkPath(chunkID string) (string, error) {
+	if chunkID == "" || filepath.Base(chunkID) != chunkID {
+		return "", ErrInvalidChunkID
+	}
+	return filepath.Join(ds.baseDir, chunkID), nil
+}
+
+func (ds *DiskStore) StoreChunk(chunkID string, data io.Reader) error {
+	ds.mu.Lock()
+	defer ds.mu.Unlock()
+
+	targetPath, err := ds.chunkPath(chunkID)
+	if err != nil {
+		return err
+	}
+
+	tempFile, err := os.CreateTemp(ds.baseDir, "upload-*")
+	if err != nil {
+		return err
+	}
+	tempName := tempFile.Name()
+
+	_, copyErr := io.Copy(tempFile, data)
+	closeErr := tempFile.Close()
+
+	if copyErr != nil {
+		os.Remove(tempName)
+		return copyErr
+	}
+	if closeErr != nil {
+		os.Remove(tempName)
+		return closeErr
+	}
+
+	return os.Rename(tempName, targetPath)
+}
+
+func (ds *DiskStore) RetrieveChunk(chunkID string) (io.ReadCloser, error) {
+	ds.mu.RLock()
+	defer ds.mu.RUnlock()
+
+	path, err := ds.chunkPath(chunkID)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, ErrChunkNotFound
+	}
+	return file, err
+}
+
+func (ds *DiskStore) DeleteChunk(chunkID string) error {
+	ds.mu.Lock()
+	defer ds.mu.Unlock()
+
+	path, err := ds.chunkPath(chunkID)
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return ErrChunkNotFound
+	}
+	return err
+}
+
+func (ds *DiskStore) HasChunk(chunkID string) (bool, error) {
+	ds.mu.RLock()
+	defer ds.mu.RUnlock()
+
+	path, err := ds.chunkPath(chunkID)
+	if err != nil {
+		return false, err
+	}
+
+	_, err = os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
+
+func (ds *DiskStore) GetStats() (int64, int64, error) {
+	ds.mu.RLock()
+	defer ds.mu.RUnlock()
+
+	var totalUsed int64
+	entries, err := os.ReadDir(ds.baseDir)
+	if err != nil {
+		return 0, ds.maxCapacity, err
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			info, err := entry.Info()
+			if err == nil {
+				totalUsed += info.Size()
+			}
+		}
+	}
+
+	return totalUsed, ds.maxCapacity, nil
+}
